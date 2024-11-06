080242fafc5321829a49691f69fba544469b1272
Polodashvili-Iosif <iosif.polodashvili@mail.ru>
2024-11-06 12:56:50 +0300

    Django project med_comfort has been created

---
+++ med_comfort/med_comfort/wsgi.py
@@ -0,0 +1,11 @@
+"""
+WSGI config for med_comfort project.
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'med_comfort.settings')
+
+application = get_wsgi_application()
