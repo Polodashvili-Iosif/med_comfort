ff6a14fd8bb2f20d0362922a895160c87604911f
Polodashvili-Iosif <iosif.polodashvili@mail.ru>
2024-12-04 10:56:43 +0300

    Add and update tests and fixtures for service list view, category detail view and service detail view

--- med_comfort/conftest.py
+++ med_comfort/conftest.py
@@ -1,7 +1,19 @@
 import pytest
 from django.core.management import call_command
 
+from services.models import Service, ServiceCategory
+
 
 @pytest.fixture
 def load_service_data(db):
     call_command('loaddata', 'services_data.json')
+
+
+@pytest.fixture
+def first_service():
+    return Service.objects.first()
+
+
+@pytest.fixture
+def first_category():
+    return ServiceCategory.objects.first()
--- med_comfort/services/tests.py
+++ med_comfort/services/tests.py
@@ -1,23 +1,21 @@
 import pytest
-from django.test import Client
 from django.urls import reverse
+from pytest_django.asserts import assertTemplateUsed
 
 from services.models import Service, ServiceCategory
 
 
 @pytest.mark.django_db
 class TestServiceListView:
-    def test_service_list_page_accessible_by_name(self, client: Client):
+    def test_page_accessible(self, client):
         response = client.get(reverse('services:services_list'))
         assert response.status_code == 200
 
-    def test_service_list_uses_correct_template(self, client: Client):
+    def test_uses_correct_template(self, client):
         response = client.get(reverse('services:services_list'))
-        assert response.templates[0].name == 'services/services_list.html'
+        assertTemplateUsed(response, 'services/services_list.html')
 
-    def test_service_list_context_data(
-        self, client: Client, load_service_data
-    ):
+    def test_context_data(self, client, load_service_data):
         response = client.get(reverse('services:services_list'))
         services = Service.objects.all()
         service_categories = ServiceCategory.objects.all()
@@ -27,3 +25,67 @@ class TestServiceListView:
             list(response.context['service_categories'])
             == list(service_categories)
         )
+
+
+@pytest.mark.django_db
+class TestCategoryDetailView:
+    def test_returns_404_for_invalid_slug(self, client):
+        invalid_slug = "non-existent-category"
+        url = reverse(
+            'services:category_detail', kwargs={'slug': invalid_slug}
+        )
+        response = client.get(url)
+        assert response.status_code == 404
+
+    def test_page_accessible(self, client, load_service_data, first_category):
+        response = client.get(reverse(
+            'services:category_detail', kwargs={'slug': first_category.slug}
+        ))
+        assert response.status_code == 200
+
+    def test_uses_correct_template(
+            self, client, load_service_data, first_category
+    ):
+        response = client.get(reverse(
+            'services:category_detail', kwargs={'slug': first_category.slug}
+        ))
+        assertTemplateUsed(response, 'services/category_detail.html')
+
+    def test_context_data(self, client, load_service_data, first_category):
+        response = client.get(reverse(
+            'services:category_detail', kwargs={'slug': first_category.slug}
+        ))
+        services = first_category.services.all()
+
+        assert list(response.context['services']) == list(services)
+
+
+@pytest.mark.django_db
+class TestServiceDetailView:
+    def test_returns_404_for_invalid_slug(self, client):
+        invalid_slug = "non-existent-service"
+        url = reverse(
+            'services:service_detail', kwargs={'slug': invalid_slug}
+        )
+        response = client.get(url)
+        assert response.status_code == 404
+
+    def test_page_accessible(self, client, load_service_data, first_service):
+        response = client.get(reverse(
+            'services:service_detail', kwargs={'slug': first_service.slug}
+        ))
+        assert response.status_code == 200
+
+    def test_uses_correct_template(
+            self, client, load_service_data, first_service
+    ):
+        response = client.get(reverse(
+            'services:service_detail', kwargs={'slug': first_service.slug}
+        ))
+        assertTemplateUsed(response, 'services/service_detail.html')
+
+    def test_context_data(self, client, load_service_data, first_service):
+        response = client.get(reverse(
+            'services:service_detail', kwargs={'slug': first_service.slug}
+        ))
+        assert response.context['service'] == first_service
