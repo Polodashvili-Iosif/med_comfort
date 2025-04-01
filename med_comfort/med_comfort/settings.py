a174d994a894621811f3e8027edf6d7081cfec72
Polodashvili-Iosif <iosif.polodashvili@mail.ru>
2025-04-01 20:26:15 +0300

    Add URLs, templates and update settings.py for password reset function

--- med_comfort/med_comfort/settings.py
+++ med_comfort/med_comfort/settings.py
@@ -120,3 +120,12 @@ AUTH_USER_MODEL = "users.User"
 LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = 'main:index'
 LOGOUT_REDIRECT_URL = 'main:index'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
--- med_comfort/users/urls.py
+++ med_comfort/users/urls.py
@@ -1,5 +1,5 @@
-from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.urls import path, reverse_lazy
 
 from . import views
 
@@ -9,12 +9,43 @@ urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
     path(
         'login/',
-        LoginView.as_view(template_name='users/login.html'),
+        auth_views.LoginView.as_view(template_name='users/login.html'),
         name='login'
     ),
     path(
         'logout/',
-        LogoutView.as_view(),
+        auth_views.LogoutView.as_view(),
         name='logout'
+    ),
+    path(
+        'password_reset/',
+        auth_views.PasswordResetView.as_view(
+            template_name='users/password_reset.html',
+            email_template_name='users/password_reset_email.html',
+            success_url=reverse_lazy("users:password_reset_done")
+        ),
+        name='password_reset'
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='users/password_reset_confirm.html',
+            success_url=reverse_lazy("users:password_reset_complete")
+        ),
+        name='password_reset_confirm'
+    ),
+    path(
+        'password_reset/done/',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='users/password_reset_done.html'
+        ),
+        name='password_reset_done'
+    ),
+    path(
+        'password_reset_complete/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='users/password_reset_complete.html'
+        ),
+        name='password_reset_complete'
     ),
 ]
