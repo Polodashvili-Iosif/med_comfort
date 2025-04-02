6ae6859461cfcc03325bbd4dd69bc9555951365a
Polodashvili-Iosif <iosif.polodashvili@mail.ru>
2025-04-02 21:16:13 +0300

    Add view and URL pattern for search page

--- med_comfort/main/views.py
+++ med_comfort/main/views.py
@@ -1,9 +1,36 @@
 from django.shortcuts import render
 
+from clinics.models import Clinic
 from doctors.models import Doctor
+from services.models import Service
 
 
 def index(request):
     doctors = Doctor.objects.all()[:6]
-    services = [doctor.services.first() for doctor in doctors]
-    return render(request, 'main/index.html', {'services': services})
+    popular_services = [doctor.services.first() for doctor in doctors]
+    return render(
+        request, 'main/index.html', {'popular_services': popular_services}
+    )
+
+
+def search(request):
+    query = request.GET.get("q", "").strip()
+
+    doctors = Doctor.objects.filter(
+        full_name__icontains=query
+    ) if query else Doctor.objects.none()
+    services = Service.objects.filter(
+        name__icontains=query
+    ) if query else Service.objects.none()
+    clinics = Clinic.objects.filter(
+        name__icontains=query
+    ) if query else Clinic.objects.none()
+
+    return render(
+        request, 'main/search.html', {
+            'query': query,
+            'doctors': doctors,
+            'services': services,
+            'clinics': clinics,
+        }
+    )
