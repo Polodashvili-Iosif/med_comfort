929dc2f8290b5f8e8b899fb4ae3d4d4e6863fae2
Polodashvili-Iosif <iosif.polodashvili@mail.ru>
2025-04-02 21:18:45 +0300

    Update views and update and add templates for search page and  related pages

--- med_comfort/services/views.py
+++ med_comfort/services/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import get_object_or_404, render
 
+from doctors.models import Doctor
+
 from .models import Service, ServiceCategory
 
 
 def services_list(request):
     service_catigories = ServiceCategory.objects.all()
     services = Service.objects.all()
+    doctors = Doctor.objects.all()[:6]
+    popular_services = [doctor.services.first() for doctor in doctors]
     context = {
         'service_categories': service_catigories,
         'services': services,
+        'popular_services': popular_services,
     }
     return render(request, 'services/services_list.html', context)
 
